22674ebc23cee7f17aa33688ebf5f8a9711bd64e
emariec <79215468+emariec@users.noreply.github.com>
2021-04-20 12:07:22 +0200

    kommentar med henvisning til teachable machine

--- stensakspapir.py
+++ stensakspapir.py
@@ -5,7 +5,7 @@ import tensorflow.keras
 from PIL import Image, ImageOps
 import random
 
-#All code regarding the ML-model (line 12-17) came with model that we trained and downloaded from https://teachablemachine.withgoogle.com/
+#All code regarding the ML-model (line 10-17 & 44-61) came with model that we trained and downloaded from https://teachablemachine.withgoogle.com/
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
