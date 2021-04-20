42fb263e33c08e73e067b61e6fce4048fb92f63f
ClaraMcNair <79084200+ClaraMcNair@users.noreply.github.com>
2021-04-20 11:38:21 +0200

    opdateret

--- stensakspapir.py
+++ stensakspapir.py
@@ -30,9 +30,9 @@ def make_move():
         # Display the resulting frame
         font = cv2.FONT_HERSHEY_SIMPLEX  
         cv2.putText(frame, count,(10,500),font,4,(255,255,255),2,cv2.LINE_AA)
-        cv2.imshow('frame',frame)
+        cv2.imshow('Rock, paper, scissor',frame)
         timePassed=time.time()
-        #print (count)
+        
         image = frame
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -42,16 +42,12 @@ def make_move():
     
     #resize the image to a 224x224 with the same strategy as in TM2:
     #resizing the image to be at least 224x224 and then cropping from the center
-    #print(image.shape)
+    
     size = (224, 224)
-    #image = ImageOps.fit(image, size, Image.ANTIALIAS)
     image = ImageOps.fit(Image.fromarray(image), size, Image.ANTIALIAS)
 
     #turn the image into a numpy array
     image_array = np.asarray(image)
-    
-    # display the resized image
-    #image.show()
     
     # Normalize the image
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
@@ -63,46 +59,59 @@ def make_move():
     prediction = model.predict(data)
     print(prediction)
    
-    moves= ['Nothing','Stone','Scissors','Paper'] #String-list with the moves' names placed at the same index, as in the model
+    moves= ['Nothing','Rock','Scissors','Paper'] #String-list with the moves' names placed at the same index, as in the model
     pred = list(prediction[0]) #list of all the values of the prediction
     
     biggest = pred.index(max(pred)) #find the index of largest value
-    #predindex = pred.index(biggest)
-    user_move = moves[biggest]#cast the users move into a string, by finding the item at the same index in our String-list of moves
     
-    
+    user_move = moves[biggest]#cast the users move into a string, by finding the item at the same index in our String-list of moves  
    
     print(user_move)
     if (user_move == 'Nothing'): #try again if no move is detected
+        cv2.putText(frame, "No move detected!",(10,150),font,2,(255,0,0),2,cv2.LINE_AA)
+        cv2.putText(frame, "Press key 0 to try again!",(10,200),font,2,(255,0,0),2,cv2.LINE_AA)
+        cv2.imshow('Rock, paper, scissor',frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         make_move()
     else:
-        Result(user_move,Computer_makemove()) #if a move is detected, run the Result()-method with the user-move and a call to Computer_makemove as its arguments.
+        Result(user_move,Computer_makemove(),frame) #if a move is detected, run the Result()-method with the user-move and a call to Computer_makemove as its arguments.
     
 #method for the computer to make a random move
 def Computer_makemove():
-   computer_move = random.choice(('Stone', 'Paper', 'Scissors'))
+   computer_move = random.choice(('Rock', 'Paper', 'Scissors'))
    print(computer_move)
    return (computer_move)
 
 #method for getting the result
-def Result(user_move, computer_move):
- 
+def Result(user_move, computer_move, frame):
+    winner = ''
 #all possible outcomes where the computer wins
-  if (user_move == 'Stone' and computer_move == 'Paper') or (user_move == 'Scissors' and computer_move == 'Stone') or (user_move == 'Paper' and computer_move == 'Scissors'): 
-        winner = "Computer"
-        print ('The winner is: ' + winner) 
+    if (user_move == 'Rock' and computer_move == 'Paper') or (user_move == 'Scissors' and computer_move == 'Rock') or (user_move == 'Paper' and computer_move == 'Scissors'): 
+            winner = "the Computer won!"
+            
+        #all possible outcomes where the user wins     
+    elif (user_move == 'Rock' and computer_move == 'Scissors') or (user_move == 'Scissors' and computer_move == 'Paper') or (user_move == 'Paper' and computer_move == 'Rock'): 
+            winner = "You won!"
+            
+    #if neither of those otcomes occur, the game is tied.        
+    else:
+        winner = "It is a TIE"
         
-    #all possible outcomes where the user wins     
-  elif (user_move == 'Stone' and computer_move == 'Scissors') or (user_move == 'Scissors' and computer_move == 'Paper') or (user_move == 'Paper' and computer_move == 'Stone'): 
-        winner = "User"
-        print ('The winner is: ' + winner)
+    font = cv2.FONT_HERSHEY_SIMPLEX  
+    cv2.putText(frame, "Computers move: " + computer_move,(10,100),font,2,(255,0,0),2,cv2.LINE_AA)
+    cv2.putText(frame, "Your move: " + user_move,(10,150),font,2,(255,0,0),2,cv2.LINE_AA)
+    cv2.putText(frame, winner,(10,250),font,4,(255,0,0),2,cv2.LINE_AA)
+    cv2.putText(frame, "Play again press key 0 " + user_move,(10,550),font,2,(255,0,0),2,cv2.LINE_AA)
+    cv2.putText(frame, "To quit press key Q " + user_move,(10,600),font,2,(255,0,0),2,cv2.LINE_AA)
+    cv2.imshow('Rock, paper, scissor', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        make_move()
+    elif cv2.waitKey(1) & 0xFF == ord('n'):
+        cv2.destroyAllWindows()
+    
         
-  #if neither of those otcomes occur, the game is tied.        
-  else:
-    winner = "It is a TIE"
-    print(winner)
-     
-
-
 make_move()
 
